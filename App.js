ac8ed08637a4455a082f71499515393355fea90f
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-05-13 22:51:09 -0300

    Tentativa - Mudança de Telas

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, Text, ScrollView ,View, TextInput, TouchableOpacity } from 'react-native';
-import gaialogo from './assets/images/1.png'
-import hexagon from './assets/images/hexagons.png'
-import seta from './assets/images/seta.png'
-import cadastroStyles from './src/styles/cadastroStyles'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/pages/login';
+import CadastroOne from './src/pages/cadastro';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={cadastroStyles.container}>
-      <Image style={[cadastroStyles.hexagon, cadastroStyles.hexagonOne]} source={hexagon}/>
-      <Image style={[cadastroStyles.hexagon, cadastroStyles.hexagonTwo]} source={hexagon}/>
-      <Image style={[cadastroStyles.hexagon, cadastroStyles.hexagonThree]} source={hexagon}/>
-      <Image style={[cadastroStyles.bigupwelcome, cadastroStyles.gcuplogo]} source={gaialogo}/>
-      <Text style={cadastroStyles.bigupwelcome}>Bem-vindo à Gaia Cup</Text>
-      <Text style={cadastroStyles.signup}>Cadastrar-se</Text>
-      <TextInput style={[cadastroStyles.insertname, cadastroStyles.inputLogin]} placeholderTextColor="#6e6e6e"  placeholder="Usuário"></TextInput>
-      <TextInput style={[cadastroStyles.insertname, cadastroStyles.inputLogin]} placeholderTextColor="#6e6e6e"  placeholder="E-mail"></TextInput>
-      <TouchableOpacity><Text style={cadastroStyles.alreadyAccount}>Eu já possuo uma conta, desejo <Text style={[cadastroStyles.yellowText]}>fazer login.</Text></Text></TouchableOpacity>
-      <TouchableOpacity style={{marginLeft: 300, marginTop: 20}}>
-        <Image source={seta} style={cadastroStyles.seta}/>
-        <Text style={cadastroStyles.whiteColor}> Continuar cadastro </Text>
-      </TouchableOpacity>
-      <Text style={[cadastroStyles.copyright, cadastroStyles.whiteColor]}>© 2022 Copyright: Inexorabilis Team</Text>
-      <StatusBar style="auto"/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Cadastro 1" component={CadastroOne} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
---
+++ src/pages/login.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Image, Text, ScrollView ,View, TextInput, TouchableOpacity } from 'react-native';
+import gaialogo from '../../assets/images/1.png'
+import hexagon from '../../assets/images/hexagons.png'
+import seta from '../../assets/images/seta.png'
+import cadastroStyles from '../styles/cadastroStyles'
+
+export default function Login({navigation}){
+     return (
+          <View style={cadastroStyles.container}>
+               <Image style={[cadastroStyles.hexagon, cadastroStyles.hexagonOne]} source={hexagon}/>
+               <Image style={[cadastroStyles.hexagon, cadastroStyles.hexagonTwo]} source={hexagon}/>
+               <Image style={[cadastroStyles.hexagon, cadastroStyles.hexagonThree]} source={hexagon}/>
+               <Image style={[cadastroStyles.bigupwelcome, cadastroStyles.gcuplogo]} source={gaialogo}/>
+               <Text style={cadastroStyles.bigupwelcome}>Bem-vindo à Gaia Cup</Text>
+               <Text style={cadastroStyles.signup}>Cadastrar-se</Text>
+               <TextInput style={[cadastroStyles.insertname, cadastroStyles.inputLogin]} placeholderTextColor="#6e6e6e"  placeholder="Usuário"></TextInput>
+               <TextInput secureTextEntry={true} style={[cadastroStyles.insertname, cadastroStyles.inputLogin]} placeholderTextColor="#6e6e6e"  placeholder="E-mail"></TextInput>
+               <TouchableOpacity >
+                    <Text style={cadastroStyles.alreadyAccount}>Eu não possuo uma conta, desejo 
+                    <Text style={[cadastroStyles.yellowText]}> fazer o cadastro.</Text>
+                    </Text>
+               </TouchableOpacity>
+               <TouchableOpacity style={{marginLeft: 300, marginTop: 20}}>
+                    <Image source={seta} style={cadastroStyles.seta}/>
+                    <Text style={cadastroStyles.whiteColor}>Continuar cadastro</Text>
+               </TouchableOpacity>
+               <Text style={[cadastroStyles.copyright, cadastroStyles.whiteColor]}>© 2022 Copyright: Inexorabilis Team</Text>
+               <StatusBar style="auto"/>
+          </View>
+     )
+}
