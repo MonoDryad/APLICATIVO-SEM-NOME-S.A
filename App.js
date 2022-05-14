ea66f4cb316696844804a4e1bd954a9031996292
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-05-13 21:18:38 -0300

    13/05 - Front-end Cadastro
    Finalizado

--- App.js
+++ App.js
@@ -3,120 +3,26 @@ import { StyleSheet, Image, Text, ScrollView ,View, TextInput, TouchableOpacity 
 import gaialogo from './assets/images/1.png'
 import hexagon from './assets/images/hexagons.png'
 import seta from './assets/images/seta.png'
+import cadastroStyles from './src/styles/cadastroStyles'
 
 export default function App() {
   return (
-    
-    <ScrollView>
-      <View style={styles.container}>
-        <Image style={[styles.hexagon, styles.hexagonOne]} source={hexagon}/>
-        <Image style={[styles.hexagon, styles.hexagonTwo]} source={hexagon}/>
-        <Image style={[styles.hexagon, styles.hexagonThree]} source={hexagon}/>
-        <Image style={[styles.bigupwelcome, styles.gcuplogo]} source={gaialogo}/>
-        <Text style={styles.bigupwelcome}>Bem-vindo à Gaia Cup</Text>
-        <Text style={styles.signup}>Cadastrar-se</Text>
-        <TextInput style={[styles.insertname, styles.inputLogin]} placeholderTextColor="#6e6e6e"  placeholder="Usuário"></TextInput>
-        <TextInput style={[styles.insertname, styles.inputLogin]} placeholderTextColor="#6e6e6e"  placeholder="E-mail"></TextInput>
-        <Text style={styles.alreadyAccount}>Eu já possuo uma conta, desejo <TouchableOpacity><Text style={[styles.alreadyAccount, styles.yellowText]}>fazer login.</Text></TouchableOpacity></Text>  
-        <TouchableOpacity style={{marginLeft: 300, marginTop: 20}}>
-          <Image source={seta} style={styles.seta}/>
-          <Text style={styles.whiteColor}> Continuar cadastro </Text>
-        </TouchableOpacity>
-        <StatusBar style="auto"/>
-      </View>
-    </ScrollView>
+    <View style={cadastroStyles.container}>
+      <Image style={[cadastroStyles.hexagon, cadastroStyles.hexagonOne]} source={hexagon}/>
+      <Image style={[cadastroStyles.hexagon, cadastroStyles.hexagonTwo]} source={hexagon}/>
+      <Image style={[cadastroStyles.hexagon, cadastroStyles.hexagonThree]} source={hexagon}/>
+      <Image style={[cadastroStyles.bigupwelcome, cadastroStyles.gcuplogo]} source={gaialogo}/>
+      <Text style={cadastroStyles.bigupwelcome}>Bem-vindo à Gaia Cup</Text>
+      <Text style={cadastroStyles.signup}>Cadastrar-se</Text>
+      <TextInput style={[cadastroStyles.insertname, cadastroStyles.inputLogin]} placeholderTextColor="#6e6e6e"  placeholder="Usuário"></TextInput>
+      <TextInput style={[cadastroStyles.insertname, cadastroStyles.inputLogin]} placeholderTextColor="#6e6e6e"  placeholder="E-mail"></TextInput>
+      <TouchableOpacity><Text style={cadastroStyles.alreadyAccount}>Eu já possuo uma conta, desejo <Text style={[cadastroStyles.yellowText]}>fazer login.</Text></Text></TouchableOpacity>
+      <TouchableOpacity style={{marginLeft: 300, marginTop: 20}}>
+        <Image source={seta} style={cadastroStyles.seta}/>
+        <Text style={cadastroStyles.whiteColor}> Continuar cadastro </Text>
+      </TouchableOpacity>
+      <Text style={[cadastroStyles.copyright, cadastroStyles.whiteColor]}>© 2022 Copyright: Inexorabilis Team</Text>
+      <StatusBar style="auto"/>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#121212',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  gcuplogo: {
-    width: 250,
-    height: 200,
-    resizeMode: 'contain',
-    marginTop: 50
-  },
-
-  bigupwelcome: {
-    fontSize: 33,
-    color: '#fff',
-    fontWeight: 'bold',
-    marginTop: 15
-  },
-
-  signup: {
-    fontSize: 24,
-    marginTop: 25,
-    color: '#fff',
-  },
-
-  insertname: {
-    color: '#fff',
-    fontSize: 20,
-    marginTop: 40, 
-  },
-
-  inputLogin:{
-    borderBottomColor: '#3b3939',
-    borderBottomWidth: 1,
-    borderLeftColor: '#3b3939',
-    borderLeftWidth: 1,
-    outlineStyle: 'none',
-    padding: 7,
-    width: 240
-  },
-
-  yellowText:{
-    color: '#ffd200'
-  },
-
-  whiteColor:{
-    color: 'white'
-  },
-
-  hexagon:{
-    position: 'absolute',
-    width: 250,
-    height: 200,
-    resizeMode: 'contain',
-  },
-
-  hexagonOne:{
-    top: -109,
-    transform: [{ rotate: '265deg' }],
-    left: -130,
-  },
-
-  hexagonTwo:{
-    top: 390,
-    transform: [{ rotate: '375deg' }],
-    left: -300,
-    width: 450,
-    height: 400,
-  },
-
-  hexagonThree:{
-    top: -79,
-    transform: [{ rotate: '235deg' }],
-    left: 290,
-    width: 200,
-    height: 150,
-  },
-
-  alreadyAccount:{
-    fontSize: 20,
-    color: '#fff',
-    marginTop: 40, 
-  },
-  
-  seta: {
-    width: 80,
-    height: 80,
-  }
-});
