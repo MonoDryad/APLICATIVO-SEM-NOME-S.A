b879366454f701bc9ceffb094221aa1325c8ddc2
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-05-20 15:20:08 -0300

    Consertado problema #1

--- App.js
+++ App.js
@@ -2,18 +2,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/pages/login';
 import Cadastro from './src/pages/cadastro';
-import {Component} from 'react';
-import { StyleSheet, Image, Text, ScrollView ,View, TextInput, TouchableOpacity, Button } from 'react-native';
-import cadastroStyles from './src/styles/cadastroStyles'
+
 
 const Stack = createNativeStackNavigator();
 
-export default App = () => {
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false, }}>
-        <Stack.Screen name="Login" component={Login} options={{ title: 'Login', animationEnabled: false, }}/>
-        <Stack.Screen name="Cadastro" component={Cadastro} options={{ title: 'Cadastro', animationEnabled: false, }}/>
+        <Stack.Screen name="Login" component={Login} options={{ title: 'Login', animationEnabled: false, animation: 'none'}}/>
+        <Stack.Screen name="Cadastro" component={Cadastro} options={{ title: 'Cadastro', animationEnabled: false, animation: 'none'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
